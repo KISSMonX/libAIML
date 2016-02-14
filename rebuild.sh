a9fd4149b0f8e3cedf743907843ec2eeadb5806e
MingH <ming.hou@robo-sys.com>
2016-02-14 17:42:57 +0800

    configure: error: "Please specify the source path, using --with-v8-path=YOUR_PATH"

--- rebuild.sh
+++ rebuild.sh
@@ -5,7 +5,7 @@ cd std_utils/
 cd ..
 
 autoreconf -i
-#./configure --enable-caiml --enable-pcrecpp
-./configure --enable-caiml --enable-aisl --enable-pcrecpp --enable-javascript --with-v8-path=/home/varol/projects/v8
+./configure --enable-caiml
+#./configure --enable-caiml --enable-aisl --enable-pcrecpp --enable-javascript --with-v8-path=/home/varol/projects/v8
 make
 
